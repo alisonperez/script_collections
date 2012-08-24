07447e9beb92f5ad3049f01b32cc7567c4d22140
alison <perez.alison@gmail.com>
2012-08-24 13:30:33 +0800

    added cp copy to Ubuntu One file

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@ mysqldump -u root -proot chits > ~/backup/rhuname.$(date +%Y%m%d).sql
 cd ~/backup
 zip -rP testingpassword rhuname.$(date +%Y%m%d).zip rhuname.$(date +%Y%m%d).sql
 cp -f rhuname.$(date +%Y%m%d).zip /media/MEMORY\ CARD/
+cp -f rhuname.$(date +%Y%m%d).zip ~/Ubuntu\ One/
 rm rhuname.$(date +%Y%m%d).sql
 echo 'Backup successful!';
 nautilus ~/backup
