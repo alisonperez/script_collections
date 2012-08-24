24230d5c10a5756caffce21c9cf80d86eeeb3b26
alison <perez.alison@gmail.com>
2012-08-24 09:46:40 +0800

    added comment on usage

--- backup.sh
+++ backup.sh
@@ -1,3 +1,6 @@
+#automated backup script with password. Alison Perez <perez.alison@gmail.com>
+#should be kept in a non-universal directory (not 0777) /var/tmp
+
 mysqldump -u root -proot chits > ~/backup/rhuname.$(date +%Y%m%d).sql
 cd ~/backup
 zip -rP testingpassword rhuname.$(date +%Y%m%d).zip rhuname.$(date +%Y%m%d).sql
